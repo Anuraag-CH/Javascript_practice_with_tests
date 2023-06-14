da1e6ae4269fd7298db685fbe3914620440d34ee
Anuraag-CH <anuraag.ch@gmail.com>
2023-06-14 21:40:48 +0530

    palindrome code

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,7 +7,36 @@
 */
 
 function isPalindrome(str) {
+  if (str.length == 0) {
+    return true
+  }
+  str = ignoreSpacesAndConvertToLower(str)
+
+  l = Math.floor(str.length / 2)
+
+  for (i = 0; i < l; i++) {
+
+    n = str.length
+
+    if (str[i] != str[n - i - 1]) {
+      return false
+    }
+
+  }
+
   return true;
 }
 
+function ignoreSpacesAndConvertToLower(str) {
+  // Remove spaces
+  const stringWithoutSpaces = str.replace(/ /g, '');
+
+  // Convert to lowercase
+  const lowercaseString = stringWithoutSpaces.toLowerCase();
+
+  return lowercaseString;
+}
+
 module.exports = isPalindrome;
+
+isPalindrome("ABccc")
